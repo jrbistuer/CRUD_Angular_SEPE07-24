69e828655fc27a08a9153557051a94ee25147355
jrbistuer <jrbistuer@gmail.com>
2024-10-03 11:43:48 +0200

    init crud

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IVacanca } from '../../model/interfaces';
+import { VacancesService } from '../../services/vacances.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, HeaderComponent],
+  imports: [RouterLink, RouterOutlet, HeaderComponent, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
 
+  testdoublebinding?: string;
+
+  nom: string = '';
+  preu: string = '';
+  pais: string = '';
+  descripcio: string = '';
+
+  vacances?: IVacanca[];
+  vacancaForm!: FormGroup;
+
+  constructor(private vacancesService: VacancesService) {}
+
   ngOnInit(): void {
     console.log('Hello World!');
+    this.getVacances();
+    this.createForm();
+  }
+
+  createForm() {
+    this.vacancaForm = new FormGroup({
+      nom: new FormControl('', [Validators.required]),
+      preu: new FormControl('', [Validators.required]),
+      pais: new FormControl(''),
+      descripcio: new FormControl('')
+    });
+  }
+
+  saveVacanca() {
+    if (this.vacancaForm.valid) {
+      const v: IVacanca = {
+        nom: this.vacancaForm.get('nom')!.value,
+        preu: +this.vacancaForm.get('preu')!.value,
+        pais:  this.vacancaForm.get('pais')!.value,
+        descripcio: this.vacancaForm.get('descripcio')!.value,
+        actiu: true
+      }
+      this.vacancesService.addVacanca(v);
+      this.getVacances();
+    } else {
+      alert('formaulario inválido');
+    }
+  }
+
+  getVacances() {
+    const v: IVacanca[] = this.vacancesService.getVacances();
+    this.vacances = v;
+  }
+
+  editVacanca(i: number) {
+    const v: IVacanca = this.vacancesService.getVacancaById(i);
+    console.log(v);    
+  }
+
+  removeVacanca(i: number) {
+    this.vacances = this.vacancesService.removeVacanca(i);
   }
 
 }
--- src/app/services/vacances.service.ts
+++ src/app/services/vacances.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { IVacanca } from '../model/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,32 @@ import { Injectable } from '@angular/core';
 export class VacancesService {
 
   constructor() { }
+
+  addVacanca(vacanca: IVacanca): boolean {
+    const vacances = this.getVacances();
+    vacances.push(vacanca);
+    this.saveVacances(vacances);
+    return true;
+  }
+
+  getVacancaById(id: number): IVacanca {
+    const vacances = this.getVacances();
+    return this.getVacances()[id];
+  }
+
+  getVacances(): IVacanca[] {
+    return JSON.parse(localStorage.getItem('vacances')!) || [];;
+  }
+
+  removeVacanca(id: number): IVacanca[] {
+    const vacances = this.getVacances();
+    vacances.splice(id, 1);
+    this.saveVacances(vacances);
+    return this.getVacances();
+  }
+
+  private saveVacances(vacances: IVacanca[]) {
+    localStorage.setItem('vacances', JSON.stringify(vacances));
+  }
+
 }
